249d1cc58c7da34af25a585dac92b2f4be767e32
Muneer Girnari <muneeragirnari@gmail.com>
2024-10-10 12:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-window.promises = [];
+window.promises = [new Promise((resolve) => {
+						setTimeout(()=>{
+							resolve("inside promise 1")
+						},3000);
+					}),
+				   new Promise((resolve) => {
+						setTimeout(()=>{
+							resolve("inside promise 2")
+						},5000);
+					}),
+				   new Promise((resolve) => {
+						setTimeout(()=>{
+							resolve("inside promise 3")
+						},1000);
+					}),
+				   new Promise((resolve) => {
+						setTimeout(()=>{
+							resolve("inside promise 4")
+						},2000);
+					}),
+				   new Promise((resolve) => {
+						setTimeout(()=>{
+							resolve("inside promise 5")
+						},4000);
+					})];
 
+Promise.any(promises).then((data) => {
+	console.log(data);
+});
 // Do not change the code above this
 // add your promises to the array `promises`
